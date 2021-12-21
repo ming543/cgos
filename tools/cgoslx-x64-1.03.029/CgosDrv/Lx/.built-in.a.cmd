3ee258a4eb2e94f2054122de9bec7e4630ad72a3
ming <ming543@gmail.com>
2021-12-21 17:52:39 +0800

    3

--- tools/cgoslx-x64-1.03.029/CgosDrv/Lx/.built-in.a.cmd
+++
@@ -1,1 +0,0 @@
-cmd_/home/production/hw_test/tools/cgoslx-x64-1.03.029/CgosDrv/Lx/built-in.a := rm -f /home/production/hw_test/tools/cgoslx-x64-1.03.029/CgosDrv/Lx/built-in.a; ar cDPrST /home/production/hw_test/tools/cgoslx-x64-1.03.029/CgosDrv/Lx/built-in.a 
